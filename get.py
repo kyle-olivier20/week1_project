dca6a7a8ef0f29d802d12341120528aa4006918a
Kyle Olivier <kyleolivier12@gmail.com>
2022-07-05 20:30:23 +0000

    fixes pt12

--- get.py
+++ get.py
@@ -24,12 +24,13 @@ def create_headers(bearer_token):
 def create_url(keyword, start_date, end_date, max_results=10):
     search_url = "https://api.twitter.com/2/tweets/search/all"
     query_params = {'query': keyword, 'start_time': start_date,\
-          'end_time': end_date,
+            'end_time': end_date,
         'max_results': max_results, 'expansions': \
         'author_id,in_reply_to_user_id,geo.place_id',
         'tweet.fields': 'id,text,author_id,\
         in_reply_to_user_id,geo,conversation_id, \
-        created_at,lang,public_metrics,referenced_tweets,reply_settings,source',
+        created_at,lang,public_metrics,referenced_tweets,\
+        reply_settings,source',
         'user.fields': 'id,name,username,created_at,description,public_metrics,verified',
         'place.fields': 'full_name,id,country,country_code,geo,name,place_type',
         'next_token': {}}
@@ -41,7 +42,7 @@ def connect_to_endpoint(url, headers, params, next_token=None):
     response = requests.request("GET", url, headers=headers, params=params)
     print("Endpoint Response Code: " + str(response.status_code))
     if response.status_code != 200:
-      raise Exception(response.status_code, response.text)
+        raise Exception(response.status_code, response.text)
     return response.json()
 
 
