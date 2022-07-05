6f4f6d7ac63b2c87454717b11dcf577e4e7d67b4
Kyle Olivier <kyleolivier12@gmail.com>
2022-07-05 20:14:48 +0000

    fixes pt5

--- get.py
+++ get.py
@@ -12,7 +12,7 @@ os.environ['TOKEN'] = 'AAAAAAAAAAAAAAAAAAAAAFEjeQEAAAAAokV07whI73HboXpYPJSzqn1e6
 
 
 def auth():
-  return os.getenv('TOKEN')
+    return os.getenv('TOKEN')
 
 
 def create_headers(bearer_token):
@@ -23,15 +23,16 @@ def create_headers(bearer_token):
 def create_url(keyword, start_date, end_date, max_results=10):
   search_url = "https://api.twitter.com/2/tweets/search/all"
   query_params = {'query': keyword, 'start_time': start_date, 'end_time': end_date,
-                    'max_results': max_results, 'expansions': 'author_id,in_reply_to_user_id,geo.place_id',
-                    'tweet.fields': 'id,text,author_id,in_reply_to_user_id,geo,conversation_id, created_at,lang,public_metrics,referenced_tweets,reply_settings,source',
-                    'user.fields': 'id,name,username,created_at,description,public_metrics,verified',
-                    'place.fields': 'full_name,id,country,country_code,geo,name,place_type',
-                    'next_token': {}}
+  'max_results': max_results, 'expansions': 'author_id,in_reply_to_user_id,geo.place_id',
+  'tweet.fields': 'id,text,author_id,in_reply_to_user_id,geo,conversation_id, created_at,lang,public_metrics,referenced_tweets,reply_settings,source',
+  'user.fields': 'id,name,username,created_at,description,public_metrics,verified',
+  'place.fields': 'full_name,id,country,country_code,geo,name,place_type',
+  'next_token': {}}
   return (search_url, query_params)
 
+
 def connect_to_endpoint(url, headers, params, next_token=None):
-  params['next_token'] = next_token
+  params['next_token']=next_token
   response = requests.request("GET", url, headers = headers, params = params)
   print("Endpoint Response Code: " + str(response.status_code))
   if response.status_code != 200:
